849367413cbe8654922768b3acd49c97e86357c0
saiabhishek-neu <itta.s@northeastern.edu>
2024-01-10 20:28:52 -0500

    update of structure

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,14 @@
 import './App.css';
 import React from 'react';
-import Sidebar from "./components/sidebar/Sidebar"
-import Home from "./components/home/Home"
-import About from './components/about/About'; 
+import Sidebar from "./components/sidebar/Sidebar";
+import Home from "./components/home/Home";
+import About from './components/about/About';
+import Portfolio from './components/portfolio/Portfolio'
+import Services from './components/services/Services';
+//import Testimonials from './components/testimonials/Testimonials';
+import Testimonials from './components/testimonials/Testimonials'
+import Resume from './components/resume/Resume';
+
 
 function App() {
   return (
@@ -11,6 +17,10 @@ function App() {
      <main className='main'>
         <Home/>
         <About/>
+        <Portfolio/>
+        <Services/>
+        <Testimonials/>
+        <Resume/>
       </main>
     </>
     )
--- frontend/src/components/sidebar/Sidebar.jsx
+++ frontend/src/components/sidebar/Sidebar.jsx
@@ -1,9 +1,28 @@
 import React from 'react';
 import "./sidebar.css";
+import Logo from "../../assets/logo.png"
 
 const Sidebar = () => {
   return (
-    <div>Sidebar</div>
+    <aside className='aside'>
+      <a href="#home" className='nav__logo'>
+      <img src={Logo} alt='sidebar_logo'/>
+      </a>
+    
+
+      <nav className='nav'>
+        <div className='nav__menu'>
+          <ul className='nav__list'>
+            <li className='nav__item'>
+              <a href="#home" className='nav__link'>
+                <i className="icon-home"></i>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      
+    </aside>
   )
 }
 
