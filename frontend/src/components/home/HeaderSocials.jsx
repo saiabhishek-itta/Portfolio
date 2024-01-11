f6efebd4f3d98acdb76557647dee79274ceeafb9
saiabhishek-neu <itta.s@northeastern.edu>
2024-01-11 16:16:48 -0500

    updated home

--- frontend/src/components/home/HeaderSocials.jsx
+++ frontend/src/components/home/HeaderSocials.jsx
@@ -3,11 +3,14 @@ import React from 'react'
 export const HeaderSocials = () => {
   return (
     <div className="home__socials">
-        <a href="" className="home__social-link" target='_blank'>
+        <a href="https://www.instagram.com" className="home__social-link" target='_blank'>
             <i class="fa-brands fa-instagram"></i>
         </a>
         <a href="" className="home__social-link" target='_blank'>
-            <i class="fa-brands fa-instagram"></i>
+            <i class="fa-brands fa-github"></i>
+        </a>
+        <a href="" className="home__social-link" target='_blank'>
+            <i class="fa-brands fa-linkedin"></i>
         </a>
 
     </div>
--- frontend/src/components/home/Home.jsx
+++ frontend/src/components/home/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import "./home.css";
 import Me from '../../assets/avatar-1.svg';
 import HeaderSocials from './HeaderSocials';
-
+import ScrollDown from './ScrollDown';
 
 const Home = () => {
   return (
@@ -11,8 +11,10 @@ const Home = () => {
         <img src={Me} alt="" className="home__img" />
         <h1 className="home__name">Sai Abhishek</h1>
         <span className="home__education">Full Stack Engineer</span>
+        <HeaderSocials/>
+        <a href="#contact" className="btn">Hire Me</a>
+        <ScrollDown/>
       </div>
-      <HeaderSocials/>
     </section>
   )
 }
