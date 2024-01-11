0ae9bde2557da4ae5c1e31dc233a509fbac68888
saiabhishek-neu <itta.s@northeastern.edu>
2024-01-11 10:23:40 -0500

    HomePage Update

---
+++ frontend/src/components/home/HeaderSocials.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export const HeaderSocials = () => {
+  return (
+    <div className="home__socials">
+        <a href="" className="home__social-link" target='_blank'>
+            <i class="fa-brands fa-instagram"></i>
+        </a>
+        <a href="" className="home__social-link" target='_blank'>
+            <i class="fa-brands fa-instagram"></i>
+        </a>
+
+    </div>
+  )
+}
+
+
+export default HeaderSocials;
--- frontend/src/components/home/Home.jsx
+++ frontend/src/components/home/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import "./home.css";
 import Me from '../../assets/avatar-1.svg';
+import HeaderSocials from './HeaderSocials';
 
 
 const Home = () => {
@@ -8,7 +9,10 @@ const Home = () => {
     <section className="home container" id="home">
       <div className="intro">
         <img src={Me} alt="" className="home__img" />
+        <h1 className="home__name">Sai Abhishek</h1>
+        <span className="home__education">Full Stack Engineer</span>
       </div>
+      <HeaderSocials/>
     </section>
   )
 }
