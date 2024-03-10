cb1c1b74e3520af14cb78c1f82e76c8eb96420ae
saiabhishek-neu <itta.s@northeastern.edu>
2024-03-10 11:58:22 -0400

    Folder Structuring

---
+++ V2/Frontend/src/components/Contact/Contact.tsx
@@ -0,0 +1,29 @@
+import { Container } from "./styles";
+import emailIcon from "../../assets/email-icon.svg";
+import phoneIcon from "../../assets/phone-icon.svg"
+import { Form } from "../Form/Form";
+
+
+export function Contact(){
+
+  return(
+    <Container id="contact">
+      <header>
+        <h2>Contact</h2>
+        <p>Ready to get started on your project? </p>
+        <p>Contact me now for a Free consultation.</p>
+      </header>
+      <div className="contacts">
+        <div>
+        <a href="mailto:saiabhishek.itta@gmail.com"><img src={emailIcon} alt="Email" /></a> 
+          <a href="mailto:saiabhishek.itta@gmail.com">saiabhishek.itta@gmail.com</a>
+        </div>
+        <div>
+        <a href="tel:+12019373609"><img src={phoneIcon} alt="Phone No" /></a>
+          <a href="tel:+12019373609">(+1) 2019373609</a>
+        </div>  
+      </div>
+      <Form></Form>
+    </Container>
+  )
+}
---
+++ V2/Frontend/src/components/Project/Project.tsx
@@ -0,0 +1,231 @@
+import { Container } from "./styles";
+// import githubIcon from "../../assets/github.svg"
+// import DownloadApp from '../../assets/download.png'
+import externalLink from "../../assets/external-link.svg"
+import ScrollAnimation from "react-animate-on-scroll";
+
+
+export function Project() {
+  return (
+    <Container id="project">
+      <h2>My Projects</h2>
+      <div className="projects">
+
+  
+
+      <ScrollAnimation animateIn="flipInX">
+          <div className="project">
+            <header>
+              <svg width="50" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#23ce6b " stroke-width="1" stroke-linecap="round" stroke-linejoin="round" ><title>Folder</title><path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path></svg>
+              {/*<div className="project-links">
+                <a href="https://kameleon.co.in/" target="_blank" rel="noreferrer">
+                  <img src={externalLink} alt="Visit site" />
+                </a>
+              </div>*/}
+            </header>
+            <div className="body">
+              <h3>URL Shortner</h3>
+              <p>
+              Developed a custom URL shortener API project, optimizing link management and enhancing user
+              experience through concise and efficient redirection
+              </p>
+            </div>
+            <footer>
+              <ul className="tech-list">
+                <li>Node JS</li>
+                <li>Express JS</li>
+                <li>Mongo DB</li>
+                <li>Firebase</li>
+                <li>NoSQL</li>
+              </ul>
+            </footer>
+          </div>
+        </ScrollAnimation>
+
+        <ScrollAnimation animateIn="flipInX">
+          <div className="project">
+            <header>
+              <svg width="50" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#23ce6b " stroke-width="1" stroke-linecap="round" stroke-linejoin="round" ><title>Folder</title><path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path></svg>
+              {/*<div className="project-links">
+                <a href="https://madewithluv.in/" target="_blank" rel="noreferrer">
+                  <img src={externalLink} alt="Visit site" />
+                </a>
+            </div>*/}
+            </header>
+            <div className="body">
+              <h3>Full Stack Travel Website</h3>
+              <p>
+              Led the development of a full stack travel website 
+              featuring robust admin and customer functionalities and
+              integrated secure payment gateway for convenient transactions
+              </p>
+            </div>
+            <footer>
+              <ul className="tech-list">
+                <li>MERN Stack</li>
+                <li>MongoDB</li>
+                <li>AWS</li>
+                <li>Heroku</li>
+              </ul>
+            </footer>
+          </div>
+        </ScrollAnimation>
+
+        <ScrollAnimation animateIn="flipInX">
+          <div className="project">
+            <header>
+              <svg width="50" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#23ce6b " stroke-width="1" stroke-linecap="round" stroke-linejoin="round" ><title>Folder</title><path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path></svg>
+              {/*<div className="project-links">
+                <a href="https://justmedicalbooks.com/" target="_blank" rel="noreferrer">
+                  <img src={externalLink} alt="Visit site" />
+                </a>
+          </div>*/}
+            </header>
+            <div className="body">
+              <h3>Store Management System</h3>
+              <p>
+              Engineered a state-of-the-art retail store management system 
+              integrating AI (Artificial Intelligence) capabilities to analyze 
+              purchase patterns and predict customer behavior
+              </p>
+            </div>
+            <footer>
+              <ul className="tech-list">
+                <li>SpringBoot</li>
+                <li>TensorFlow</li>
+                <li>jQuery</li>
+                <li>PostgreSQL</li>
+              </ul>
+            </footer>
+          </div>
+        </ScrollAnimation>
+
+        {/* <ScrollAnimation animateIn="flipInX">
+          <div className="project">
+            <header>
+              <svg width="50" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#23ce6b " stroke-width="1" stroke-linecap="round" stroke-linejoin="round" ><title>Folder</title><path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path></svg>
+              <div className="project-links">
+                <a href="https://www.smartcornersproperties.com/" target="_blank" rel="noreferrer">
+                  <img src={externalLink} alt="Visit site" />
+                </a>
+              </div>
+            </header>
+            <div className="body">
+              <h3>SmartCorners - Real Estate Properties Site</h3>
+              <p>
+              This real estate site displays land properties and helps buyers and sellers. Buyers can see lots of pictures, videos, and details about properties before visiting in person. It makes it easier for buyers to find what they want and helps sellers get more attention.
+              </p>
+            </div>
+            <footer>
+              <ul className="tech-list">
+                <li>Wordpress</li>               
+              </ul>
+            </footer>
+          </div>
+        </ScrollAnimation> 
+
+        <ScrollAnimation animateIn="flipInX">
+          <div className="project">
+            <header>
+              <svg width="50" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#23ce6b " stroke-width="1" stroke-linecap="round" stroke-linejoin="round" ><title>Folder</title><path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path></svg>
+              <div className="project-links">
+                <a href="https://drrohitdamor.com" target="_blank" rel="noreferrer">
+                  <img src={externalLink} alt="Visit site" />
+                </a>
+             </div>
+            </header>
+            <div className="body">
+              <h3>Dr.Rohit Damor - Online Appointment Booking Site</h3>
+              <p>
+              This website allows people to easily book appointments with you online. It takes just a minute to schedule a meeting. This kind of site is helpful for booking appointments and not missing any potential clients. You should try this website to see how convenient and efficient it is.
+              </p>
+            </div>
+            <footer>
+              <ul className="tech-list">
+                <li>Wordpress</li>
+              </ul>
+            </footer>
+          </div>
+        </ScrollAnimation>
+
+        
+        <ScrollAnimation animateIn="flipInX">
+          <div className="project">
+            <header>
+              <svg width="50" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#23ce6b " stroke-width="1" stroke-linecap="round" stroke-linejoin="round" ><title>Folder</title><path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path></svg>
+              <div className="project-links">
+                <a href="https://velocityai.com/" target="_blank" rel="noreferrer">
+                  <img src={externalLink} alt="Visit site" />
+                </a>
+              </div>
+            </header>
+            <div className="body">
+              <h3>VeloCityAI</h3>
+              <p>
+              VelocityAI is a website that allows users to get instant answers to any question and generate stunning AI pictures using OpenAI's GPT-3 and DALL-E model.With VelocityAI, users can type in text-based prompts and receive a corresponding response.
+              </p>
+            </div>
+            <footer>
+              <ul className="tech-list">
+                <li>React</li>
+                <li>Typescript</li>
+                <li>Html</li>
+                <li>css</li>
+              </ul>
+            </footer>
+          </div>
+        </ScrollAnimation>
+
+         <ScrollAnimation animateIn="flipInX">
+          <div className="project">
+            <header>
+              <svg width="50" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#23ce6b " stroke-width="1" stroke-linecap="round" stroke-linejoin="round" ><title>Folder</title><path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path></svg>
+              <div className="project-links">
+                <a href="https://mitulindustries.com/" target="_blank" rel="noreferrer">
+                  <img src={externalLink} alt="Visit site" />
+                </a>
+              </div>
+            </header>
+            <div className="body">
+              <h3>Mitul Industries - Manufacturing Industries Site</h3>
+              <p>
+              This manufacturing industry website shows off products and helps the business get more customers and information online. Making products is important but making money from them is the goal. This site can help the business make more profit.
+              </p>
+            </div>
+            <footer>
+              <ul className="tech-list">
+                <li>Wordpress</li>
+              </ul>
+            </footer>
+          </div>
+        </ScrollAnimation>
+
+        <ScrollAnimation animateIn="flipInX">
+          <div className="project">
+            <header>
+              <svg width="50" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#23ce6b " stroke-width="1" stroke-linecap="round" stroke-linejoin="round" ><title>Folder</title><path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path></svg>
+              <div className="project-links">
+                <a href="https://mayatmaj.com/" target="_blank" rel="noreferrer">
+                  <img src={externalLink} alt="Visit site" />
+                </a>
+              </div>
+            </header>
+            <div className="body">
+              <h3>Mayatmaj - Film Production Site</h3>
+              <p>
+              This agency website promotes post production, virtual production, and video marketing services. It shows previous client work and reviews to get more business. If you need help with production, this website can give you information and connect you with the right people.
+              </p>
+            </div>
+            <footer>
+              <ul className="tech-list">
+                <li>Wordpress</li>              
+              </ul>
+            </footer>
+          </div>
+        </ScrollAnimation>
+       */}
+
+      </div>
+    </Container>
+  );
+}
